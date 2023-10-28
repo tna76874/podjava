f9cb5b8f090a92bbfbed7814d615a91bd7da0385
lmh <dev@hilberg.eu>
2023-10-28 11:16:06 +0200

    add install.sh

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function check_for_podman(){
+if ! command -v podman &> /dev/null; then
+    sudo apt update > /dev/null 2>&1
+    echo "Podman not found. Installing..."
+    sudo apt -y install podman
+    if [ $? -eq 0 ]; then
+        echo "Podman installation successful."
+    else
+        echo "Error installing Podman. Adding ppa"
+        return 1
+    fi
+else
+    echo "Podman is already installed."
+fi
+}
+
+function add_podman_ppa_and_podman() {
+    . /etc/os-release
+    echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+    curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add -
+    sudo apt update &> /dev/null
+    sudo apt -y install podman &> /dev/null
+}
+
+function ensure_executeable() {
+    executablepath="/usr/local/bin/podjava"
+    echo 'podman run -it -v "$PWD":/usr/src/myapp -w /usr/src/myapp docker.io/library/openjdk:18 $@' | sudo tee $executablepath > /dev/null
+    sudo chmod +x $executablepath
+}
+
+check_for_podman || add_podman_ppa_and_podman && ensure_executeable
